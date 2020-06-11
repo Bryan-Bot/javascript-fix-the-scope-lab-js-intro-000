ac1aae0b3d27a37130dc67fcf1dbe32923afbfd2
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-06-11 22:41:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var animal = 'dog'
 
 function myAnimal() {
   // You should not need to modify this function
-  animal = 'cat'
   return animal
 }
 
@@ -12,6 +11,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
+  animal = 'cat'
   return animal
 }
 
